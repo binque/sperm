3a8267020825a7af254b3351ef81375d542e1643
dirtysalt <dirtysalt1987@gmail.com>
2012-07-27 18:21:35 +0800

    [gc]commit

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ TARGETS="2utf8 cxxindent \
 oprof mysqldb syslog \
 local-install org2twiki \
  gc pom-create vpn-umeng vpn-gfw
-rhs pcrypt einstall"
+rhs pcrypt einstall uinstall"
 
 INSTALLDIR=$HOME/utils/bin
 if [ ! -d $INSTALLDIR ]
