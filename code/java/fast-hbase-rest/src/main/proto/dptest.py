94b7f263df73ad55e1ced1e10f82bc73dc28aa59
dirtysalt <dirtysalt1987@gmail.com>
2013-02-22 15:24:48 +0800

    add some test

--- code/java/fast-hbase-rest/src/main/proto/dptest.py
+++ code/java/fast-hbase-rest/src/main/proto/dptest.py
@@ -41,7 +41,33 @@ def queryColumnFamily():
     response = message_pb2.ReadResponse()
     response.ParseFromString(data2)
     #print response
+
+def multiQuery():
+    print '----------multiQuery----------'
+    mRequest = message_pb2.MultiReadRequest()
+
+    request = message_pb2.ReadRequest()
+    request.table_name='appbenchmark'
+    request.row_key='2012-04-08_YULE'
+    request.column_family='stat'
+    request.qualifiers.append('14_day_active_count_avg')
+    mRequest.requests.extend([request])
+
+    request = message_pb2.ReadRequest()
+    request.table_name='appbenchmark'
+    request.row_key='2012-04-08_YULE'
+    request.column_family='stat'
+    mRequest.requests.extend([request])
+
+    data = mRequest.SerializeToString()
+    data2 = raiseHTTPRequest('http://dp0:12345/multi-read',data,timeout=20)
+    
+    mResponse = message_pb2.MultiReadResponse()
+    mResponse.ParseFromString(data2)
+    #print mResponse
+
     
 if __name__=='__main__':
     queryColumn()
     queryColumnFamily()
+    multiQuery()
