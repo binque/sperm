82a5b2b625ec4fc9e98ac28cdc3c453c9c884396
dirtysalt <dirtysalt1987@gmail.com>
2013-03-13 10:29:23 +0800

    [gc]commit

--- code/java/playboard/map_perf_test.cc
+++ code/java/playboard/map_perf_test.cc
@@ -17,7 +17,10 @@ static inline double gettime_ms() {
 
 static const int NUMBER = 10000000;
 static const char* PREFIX = "s";
-static void action(map<string, long>& dict) {
+
+static void action() {
+  double start = gettime_ms();  
+  map<string, long> dict;
   char buf[64];
   char buf2[64];
   for(int i = 0; i < NUMBER; i++) {
@@ -29,13 +32,26 @@ static void action(map<string, long>& dict) {
     snprintf(buf2, sizeof(buf2), "%s%d", PREFIX, (i + 1000) % NUMBER);
     dict[buf] += dict[buf2];
   }
+  double end = gettime_ms();
+  printf("%.2lf\n", end - start);
+}
+
+static void action2() {
+  printf("enter...\n");
+  double start = gettime_ms();  
+  map<int, long> dict;
+  for(int i = 0; i < NUMBER; i++) {
+    dict[i] = i;
+  }
+  for(int i = 0; i < NUMBER; i++) {
+    dict[i] += dict[(i + 1000) % NUMBER];
+  }
+  double end = gettime_ms();
+  printf("exit...%.2lf\n", end - start);  
 }
 
 int main() {
-  double start = gettime_ms();
-  map<string, long> dict;
-  action(dict);
-  double end = gettime_ms();
-  printf("%.2lf\n", end - start);
+  action();
+  action2();
   return 0;
 }
--- code/java/playboard/src/main/java/com/dirlt/java/playboard/MapPerformanceTest.java
+++ code/java/playboard/src/main/java/com/dirlt/java/playboard/MapPerformanceTest.java
@@ -14,7 +14,8 @@ public class MapPerformanceTest {
     private static final int NUMBER = 10000000;
     private static final String PREFIX = "s";
 
-    public static void action(Map<String, Long> dict) {
+    public static void action() {
+        Map<String, Long> dict = new HashMap<String, Long>();
         for (int i = 0; i < NUMBER; i++) {
             dict.put(PREFIX + i, (long) i);
         }
@@ -23,11 +24,25 @@ public class MapPerformanceTest {
         }
     }
 
+    public static void action2() {
+        Map<Integer, Long> dict = new HashMap<Integer, Long>();
+        for (int i = 0; i < NUMBER; i++) {
+            dict.put(i, (long) i);
+        }
+        for (int i = 0; i < NUMBER; i++) {
+            dict.put(i, dict.get(i) + dict.get((i + 1000) % NUMBER));
+        }
+    }
+
     public static void main(String[] args) {
-        Map<String, Long> dict = new HashMap<String, Long>();
         long start = System.currentTimeMillis();
-        action(dict);
+        action();
         long end = System.currentTimeMillis();
+        System.out.printf("%.2f\n", (float) (end - start));
+
+        start = System.currentTimeMillis();
+        action2();
+        end = System.currentTimeMillis();
         System.out.printf("%.2f\n", (float) (end - start));
     }
 }
